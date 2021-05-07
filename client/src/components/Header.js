506c78aad75fb58c66bb516618398854e6e1fa62
KuBerlinON <bkuberlinov@gmail.com>
2021-05-07 16:26:35 +0200

    bug fixed, private and public routes and  conditional rendering of header is finished

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,17 +3,28 @@ import { NavLink } from 'react-router-dom';
 import { startLogout } from '../actions/auth';
 import { connect } from 'react-redux';
 
-export const Header = ({ startLogout }) => (
-    <header>
-        <NavLink activeClassName='is-active' className='nav-link' to='/home' exact={true}>Home</NavLink>
-        <NavLink activeClassName='is-active' className='nav-link' to='/notes'>Notes</NavLink>
-        <NavLink activeClassName='is-active' className='nav-link' to='/about'>Organizing your studies</NavLink>
-        <button onClick={startLogout} >Logout</button>
-    </header>
+export const Header = ({ startLogout, isAuthenticated }) => (
+    isAuthenticated ? (
+        <header id='private-hd'>
+            <NavLink activeClassName='is-active' className='nav-link' to='/home' exact={true}>Home</NavLink>
+            <NavLink activeClassName='is-active' className='nav-link' to='/notes'>Notes</NavLink>
+            <NavLink activeClassName='is-active' className='nav-link' to='/about'>About</NavLink>
+            <button onClick={startLogout} >Logout</button>
+        </header>
+    ) : (
+        <header id='public-hd'>
+            <NavLink activeClassName='is-active' className='nav-link' to='/' exact={true}>Login</NavLink>
+            <NavLink activeClassName='is-active' className='nav-link' to='/about'>About</NavLink>
+        </header>
+    )
 );
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: !!state.auth.uid
+})
 
 const mapDispatchToProps = (dispatch) => ({
     startLogout: () => { dispatch(startLogout()) }
 })
 
-export default connect(undefined, mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
